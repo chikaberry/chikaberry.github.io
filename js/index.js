19fa44298e9bff6a0ab628a942621de002016f25
chikaberry <79671470+chikaberry@users.noreply.github.com>
2021-06-09 20:16:51 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.querySelector('#lastmod').textContent = document.lastModified;
+
+let currentYear = "2021";
+let yearElement = document.querySelector('#year')
+yearElement.textContent = currentYear;
