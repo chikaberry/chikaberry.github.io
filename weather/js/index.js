080019ea39f32a944399b1f4e8b59d26d9e0955d
chikaberry <79671470+chikaberry@users.noreply.github.com>
2021-06-20 10:43:10 -0600

    initial commit

--- weather/js/index.js
+++ weather/js/index.js
@@ -3,3 +3,10 @@ document.querySelector('#lastmod').textContent = document.lastModified;
 let currentYear = "2021";
 let yearElement = document.querySelector('#year')
 yearElement.textContent = currentYear;
+
+
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
