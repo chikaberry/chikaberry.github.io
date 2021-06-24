f7e17c6129ea4c32398cd5de5075c48cecee19fd
chikaberry <79671470+chikaberry@users.noreply.github.com>
2021-06-24 16:31:54 -0600

    initial change

--- weather/js/index1.js
+++ weather/js/index1.js
@@ -1,3 +1,20 @@
+
+let thedate = new Date();
+console.thedate
+if (thedate.getDay() == 5) {
+    document.querySelector('.banner').style.display = 'block';
+
+
+} else{
+    document.querySelector('.banner').style.display = 'none';
+}
+
+/*the =display else helps to not display it if it doesnt meet the condition.*/
+
+
+
+
+
 document.querySelector('#lastmod').textContent = document.lastModified;
 
 let currentYear = "2021";
@@ -5,8 +22,18 @@ let yearElement = document.querySelector('#year')
 yearElement.textContent = currentYear;
 
 
+document.querySelector(".banner__close").addEventListener("click", function () {
+  this.closest(".banner").style.display = "none";
+});
+
+
+
+
+
 
 function toggleMenu() {
     
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+
